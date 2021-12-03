bbe7402568a0215a674a1fe21beca9452b826f9c
Stanislav <t0988348363@gmail.com>
2021-12-04 04:42:58 +0700

    start class for work with CSV

--- storage/database.py
+++ storage/database.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 
+
 class Database:
-    def __init__(self):
+    def __init__(self, name):
+        self.db = pd.read_csv(name, delimiter=',')
+
+    def put(self):
+        self.db.head()
+
+
+
+
