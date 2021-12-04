1e77fd12a25f46127f142cbd98f2a8ea819b0a7a
AlelV <89650482728@mail.ru>
2021-12-04 15:30:58 +0300

    pic

--- quiz/quizpic.py
+++ quiz/quizpic.py
@@ -1,20 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
+category = [['Team 1', 100, 200, 300, 400],
+     ['Team 2', 100, 200, 300, 400],
+     ['Team 3', 100, '', 300, 400],
+     ['Team 4', 100, 200, '', 400],
+     ['Team 5', '', 200, 300, 400]]
 
-d = {'category 1': [100,200,300,400],
-     'category 2': [100,200,300,400],
-     'category 3': [100,200,300,400],
-     'category 4': [100,200,300,400],
-     'category 5': [100,200,300,400],
-     }
 
 h = 300
-w = 420
 k = 7
 step = int(h / 5)
 w = step * k
 
 im = Image.new('RGB', (w, h), (49,140,231))
 draw = ImageDraw.Draw(im)
+
 for i in range(0, 6):
   draw.line((0, step * i, w, step * i), fill='white', width=1)
 
@@ -23,10 +22,11 @@ for i in range(3, k+1):
 
 draw.line((0, 0, 0, h), fill='white', width=1)
 
-for key, i in zip(d.keys(), range(0, k)):
-  draw.text((10,step*i+int(step/2)-5), key, font = None, fill=(255,255,255,0))
-  for j, n in zip(d[key], range(3, k+1)):
-    draw.text((step*n+int(step/2)-5, step*i+int(step/2)-5), str(j), font = None, fill=(255,255,255,0))
+for cat, i in zip(category, range(0, k)):
+  draw.text((10,step*i+int(step/2)-5), str(cat[0]), font = None, fill=(255,255,255,0))
+  for question, n in zip(cat[1:], range(3, k+1)):
+    draw.text((step*n+int(step/2)-5, step*i+int(step/2)-5), str(question), font = None, fill=(243,244,175,0))
+
 
 im.show()
 
