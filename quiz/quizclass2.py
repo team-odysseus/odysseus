202c07f4ea364cb489a00a4b2694a023e6bce1e1
cubecloud <rainmaverick@mail.ru>
2021-12-04 03:48:52 +0300

    bug repair

---
+++ quiz/quizclass2.py
@@ -0,0 +1,41 @@
+import random
+import numpy as np
+import pandas as pd
+
+from storage.database import LoadData
+
+
+class Quiz(object):
+    def __init__(self):
+        self.ld = LoadData()
+        self.categories_num: int = 5
+        self.unq_categories = np.arange(self.categories_num)
+        pass
+
+    def get_one_cat_questions(self):
+
+        pass
+
+
+    def choose_categories(self):
+        categories = self.ld.get_column('cat_num')
+        unq_categories, unq_counts = np.unique(categories, return_counts=True)
+        unq_categories = unq_categories[unq_counts >= 4]
+        self.unq_categories = random.sample(unq_categories, k=self.categories_num)
+        return unq_categories
+
+    def prepare_questions(self):
+
+        return
+
+    def create_5x4_question_box(self):
+        pass
+
+if __name__ == "__main__":
+
+    q = Quiz(5, 4)
+    q
+
+
+
+
