3165974943052c0e401bd57ea4bf44b6010ec181
Mitek <big.pog@gmail.com>
2021-12-05 11:36:10 +0300

    csv parsing fix, remove prints

--- game/csv_reader.py
+++ game/csv_reader.py
@@ -15,18 +15,18 @@ def load_rounds():
     choices_d = []
     choice_d = dict()
 
-    data = pd.read_csv('../tests/test_scenario.csv')
+    data = pd.read_csv('../tests/test_scenario_2.csv')
     for r_number in range(1, data.round_index.max()+1):
         d = data[data.round_index == r_number]
         for role_i in ROLE_GOOD, ROLE_BAD:
             for choice_i in range(d.index.min(), d.index.max()+1):
-                choice_d[TEXT_KEY] = d[role_i+'.'+CHOISES_KEY+'.'+TEXT_KEY][choice_i]
-                choice_d[STAT_1_KEY] = d[role_i+'.'+CHOISES_KEY+'.'+STAT_1_KEY][choice_i]
+                choice_d[TEXT_KEY] = d[role_i+'_'+CHOISES_KEY+'_'+TEXT_KEY][choice_i]
+                choice_d[STAT_1_KEY] = d[role_i+'_'+CHOISES_KEY+'_'+STAT_1_KEY][choice_i]
                 choices_d.append(dict(choice_d))
                 choice_d.clear()
             role_d[CHOISES_KEY] = list(choices_d)
             choices_d = []
-            role_d[TEXT_KEY] = d[role_i+'.'+TEXT_KEY][d.index.min()]
+            role_d[TEXT_KEY] = d[role_i+'_'+TEXT_KEY][d.index.min()]
             round_d[role_i] = dict(role_d)
             role_d.clear()
         round_d[COM_DESCR_KEY] = d.common_description[d.index.min()]
